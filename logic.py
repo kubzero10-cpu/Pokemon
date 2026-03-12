16ec63ef7d6bd7ba088c087a7d80774ac90331d9
вашÐkubzero10-cpu <kubzero.1.0@gmail.com>
2026-03-12 16:14:44 +0300

    Initial commit: Pokemon bot with type property

--- logic.py
+++ logic.py
@@ -1,23 +1,32 @@
 from random import randint
 import requests
 
+
 class Pokemon:
     pokemons = {}
+
     # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer):
 
-        self.pokemon_trainer = pokemon_trainer   
+        self.pokemon_trainer = pokemon_trainer
 
-        self.pokemon_number = randint(1,1000)
+        self.pokemon_number = randint(1, 1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.type = self.get_type()  #  новое свойство
 
         Pokemon.pokemons[pokemon_trainer] = self
 
     # Метод для получения картинки покемона через API
     def get_img(self):
-        pass
-    
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']["other"]['dream world']["front_default"])
+        else:
+            return "https://static.wikia.nocookie.net/pokemon/images/0/0d/025Pikachu.png/revision/latest/scale-to-width-down/1000?cb=20181020165701&path-prefix=ru"
+
     # Метод для получения имени покемона через API
     def get_name(self):
         url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
@@ -28,14 +37,20 @@ class Pokemon:
         else:
             return "Pikachu"
 
+    # Новый метод для получения типа покемона
+    def get_type(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if data['types']:
+                return data['types'][0]['type']['name'].capitalize()
+        return "Normal"
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покеомона: {self.name}\nТип: {self.type}"
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
-
-
-
